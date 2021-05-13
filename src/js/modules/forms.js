1e78dd3cff9d0043d6fdbfe8c7099f0115b412ab
Антон Агаронов <antonagaronov@MacBook-Air-Anton.local>
2021-05-13 14:33:56 +0400

    feat: add forms function

---
+++ src/js/modules/forms.js
@@ -0,0 +1,113 @@
+// import checkNumInputs from './checkNumInputs';
+
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input'),
+        upload = document.querySelectorAll('[name="upload"]');
+        
+    // checkNumInputs('input[name="user_phone"]');    
+    
+    const message = {
+        loading: 'Загрузка',
+        succes: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так',
+        spinner: 'assets/img/spinner.gif',
+        ok: 'assets/img/ok.png',
+        fail: 'assets/img/fail.png',
+    };
+
+    //Пути для отправки данных из форм (текст и изображения по разным адресам)
+    const path = {
+        designer: 'assets/server.php',
+        question: 'assets/question.php'
+    };
+
+    //Функция postData является запросом на сервер
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: "POST",
+            body: data,
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+        upload.forEach(item => {
+            item.previousElementSibling.textContent = "Файл не выбран";
+        });
+    };
+
+    //Изменяем текст о загрузке изображения, с изменением имени файла в зависимости от длины
+    upload.forEach(item => {
+        item.addEventListener('input', () => {
+            console.log(item.files[0]);
+            let dots;
+            const arr = item.files[0].name.split('.');
+            //'dsfnjsdfjsdf.jpg' => ['dsfnjsdfjsdf', 'jpg']
+            arr[0].length > 6 ? dots = "..." : dots = '.';
+            const name = arr[0].substring(0, 6) + dots + arr[1];
+            item.previousElementSibling.textContent = name;
+        });
+    });
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+            //Создаем блок со статусом сообщения отправки формы и помещаем его в конец перебираемой формы
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.parentNode.appendChild(statusMessage);
+
+            //через 400 мс после отправки форма убирается со страницы
+            item.classList.add('animated', 'fadeOutUp');
+            setTimeout(() => {
+                item.style.display = 'none';
+            }, 400);
+
+            let statusImg = document.createElement('img');
+            statusImg.setAttribute('scr', message.spinner);
+            statusImg.classList.add('animated', 'fadeInUp');
+            statusMessage.appendChild(statusImg);
+
+            let textMessage = document.createElement('div');
+            textMessage.textContent = message.loading;
+            statusMessage.appendChild(textMessage);
+
+            // Собираем данные из формы при помощи объекта formData
+            const formData = new FormData(item);
+            let api;
+            //Если форма содержит картинку  - формируем путь для дизайна
+            item.closest('.popup-design') || item.classList.contains('calc_form') ? api = path.designer : api = path.question;
+            console.log(api);
+
+        
+
+            postData(api, formData)
+                .then(res => {
+                    console.log(res);
+                    statusImg.setAttribute('src', message.ok);
+                    textMessage.textContent = message.succes;
+                })
+                .catch(() => {
+                    statusImg.setAttribute('src', message.fail);
+                    textMessage.textContent = message.failure;
+                })
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                        item.style.display = 'block';
+                        item.classList.remove('fadeOutUp');
+                        item.classList.add('fadeInUp');
+                    }, 5000);
+                });
+
+        });
+    });
+}
+
+export default forms;
