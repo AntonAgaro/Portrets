537b28cb2a9d6f88a0b92056d83b83783fb401ad
Антон Агаронов <antonagaronov@MacBook-Air-Anton.local>
2021-05-13 15:53:10 +0400

    feat: add phone mask, add only rus letters validation

---
+++ src/js/modules/checkTextInputs.js
@@ -0,0 +1,15 @@
+//Возможность вводить только буквы русского алфавита
+
+const checkTextInputs = (selector) => {
+    const txtInputs = document.querySelectorAll(selector);
+
+    txtInputs.forEach(input => {
+        input.addEventListener('keypress', (e) => {
+            if (e.key.match(/[^а-яё 0-9]/ig)) {
+                e.preventDefault();
+            }
+        }); 
+    });
+};
+
+export default checkTextInputs;
