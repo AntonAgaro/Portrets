71a660318232c5487821baf5835a834bdf3ab8bb
Антон Агаронов <antonagaronov@MacBook-Air-Anton.local>
2021-05-15 13:50:24 +0400

    feat: users notification about send image in up form

--- src/js/modules/drop.js
+++ src/js/modules/drop.js
@@ -32,7 +32,10 @@ const drop = () => {
         item.closest('.file_upload').style.border = "none";
         if (item.closest('.calc_form')) {
             item.closest('.file_upload').style.backgroundColor = "#fff";
-        } else {
+        } else if (item.id == 'drop') {
+            item.closest('.file_upload').style.backgroundColor = "#f7e7e6";
+        } 
+        else {
             item.closest('.file_upload').style.backgroundColor = "#ededed";
         }
         
@@ -52,20 +55,27 @@ const drop = () => {
 
     fileInputs.forEach(input => {
         input.addEventListener('drop', (e) => {
-            input.files = e.dataTransfer.files;
-            let dots;
-            const arr = input.files[0].name.split('.');
-            arr[0].length > 6 ? dots = "..." : dots = '.';
-            const name = arr[0].substring(0, 6) + dots + arr[1];
-            input.previousElementSibling.textContent = name;
-
-            if (input.matches('#drop')) {
+            if (input.id == 'drop') {
                 console.log(input.files[0]);
                 postData('assets/server.php', input.files[0])
                     .then(res => {
                         console.log(res);
+                        input.previousElementSibling.textContent = 'Отправлено!';
+                        setTimeout(() => {
+                            input.previousElementSibling.textContent = 'Файл не выбран';
+                        }, 3000);
                     });
+            } 
+            else {
+                input.files = e.dataTransfer.files;
+                let dots;
+                const arr = input.files[0].name.split('.');
+                arr[0].length > 6 ? dots = "..." : dots = '.';
+                const name = arr[0].substring(0, 6) + dots + arr[1];
+                input.previousElementSibling.textContent = name;
+
             }
+    
         });
     });
 
